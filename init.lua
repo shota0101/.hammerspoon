57719ad0a75569051c972187afe2bfac1ed09875
Shota Hayashi <shota.hayashi.0101@gmail.com>
2023-08-04 22:37:04 +0900

    優先するブラウザを変更

--- init.lua
+++ init.lua
@@ -162,11 +162,11 @@ end)
 -- 1 : iTerm
 launcher({'cmd', 'ctrl'}, '2', 'Google Chrome')
 launcher({'cmd', 'ctrl'}, '3', 'Vivaldi')
-launcher({'cmd', 'ctrl'}, '4', 'Opera')
-launcher({'cmd', 'ctrl'}, '5', 'Helium')
-launcher({'cmd', 'ctrl'}, '6', 'Brave Browser')
-launcher({'cmd', 'ctrl'}, '7', 'Safari')
-launcher({'cmd', 'ctrl'}, '8', 'Firefox')
+launcher({'cmd', 'ctrl'}, '4', 'Firefox')
+launcher({'cmd', 'ctrl'}, '5', 'Opera')
+launcher({'cmd', 'ctrl'}, '6', 'Helium')
+launcher({'cmd', 'ctrl'}, '7', 'Brave Browser')
+launcher({'cmd', 'ctrl'}, '8', 'Safari')
 
 -- a : Reminders
 launcher({'cmd', 'ctrl'}, 's', 'Microsoft To Do')
